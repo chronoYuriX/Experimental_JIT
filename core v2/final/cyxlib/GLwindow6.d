7d05f4fa3004a09fc099cacbaf15294c4df1ad94
chronoYuriX <224791264+chronoYuriX@users.noreply.github.com>
2026-09-06 01:43:27 +0800

    commit No.8.2
    Less files!

--- core v2/final/cyxlib/GLwindow6.d
+++
@@ -1,6 +0,0 @@
-X:\Projects\JIT\core\ v2\final\cyxlib\GLwindow6.exe: \
- X:\Projects\JIT\core\ v2\final\cyxlib\GLwindow6.cpp \
- X:\Projects\JIT\core\ v2\final\cyxlib\io.cpp \
- X:\Projects\JIT\core\ v2\final\cyxlib\non-crt-math.cpp
-X:\Projects\JIT\core\ v2\final\cyxlib\io.cpp:
-X:\Projects\JIT\core\ v2\final\cyxlib\non-crt-math.cpp:
